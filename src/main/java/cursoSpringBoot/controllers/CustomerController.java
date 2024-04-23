0821d097a21bc3caa0e10597dbd42fd51200ee0b
FelipeHerreraPinzon <felipeherrerapinzon@gmail.com>
2024-04-23 03:14:30 -0500

    first commit

---
+++ src/main/java/cursoSpringBoot/controllers/CustomerController.java
@@ -0,0 +1,26 @@
+package cursoSpringBoot.controllers;
+
+import cursoSpringBoot.domain.Customer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CustomerController {
+
+    private List<Customer> customers = new ArrayList<>(Arrays.asList(
+            new Customer(123, "Gerardo López", "gerardol","contrasena123"),
+            new Customer(456, "Alejandra García", "alegarcia", "clave456"),
+            new Customer(789, "Laura Sanchéz", "lauras", "secreto789"),
+            new Customer(234, "Carlos Martínez", "carlosm", "password234")
+    ));
+
+    @GetMapping("clientes")
+    public List<Customer> getCustomers(){
+        return customers;
+    }
+
+}
